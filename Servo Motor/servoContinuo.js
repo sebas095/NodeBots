79e4e9bb79c2f39be5107f10c6e33111a15c166f
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-04-27 00:44:10 -0500

    Add servo-continuous

---
+++ Servo Motor/servoContinuo.js
@@ -0,0 +1,36 @@
+const five = require('johnny-five');
+const keypress = require('keypress');
+
+keypress(process.stdin);
+
+const board = new five.Board();
+
+board.on('ready', function() {
+  console.log("Use Up and Down arrows for CW and CCW respectively. Space to Stop.");
+
+  var servo = new five.Servo.Continuous(10);
+
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('keypress', function(ch, key) {
+    if (!key) return;
+    if (key.name === "q") {
+      console.log("Exit");
+      process.exit();
+    }
+    else if (key.name === "up") {
+      console.log("CW");
+      servo.cw();
+    }
+    else if (key.name === "down") {
+      console.log("CCW");
+      servo.ccw();
+    }
+    else if (key.name === "space") {
+      console.log("Stopping");
+      servo.stop();
+    }
+  });
+});
