4078a86a16710d510f3e988fc4b0478e07800423
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-05-04 11:06:45 -0500

    Some fixed

--- Motor Paso a Paso/myStepper.js
+++ Motor Paso a Paso/myStepper.js
@@ -1,5 +1,5 @@
 function Stepper(state) {
-  this.state = state;
+  var nstate = state;
   var current = 0;
 
   this.step = function self(i, n) {
@@ -8,49 +8,49 @@ function Stepper(state) {
       i++;
       switch(current) {
         case 0:{
-          this.state[0].off();
-          this.state[1].off();
-          this.state[2].off();
-          this.state[3].on();
+          nstate[0].off();
+          nstate[1].off();
+          nstate[2].off();
+          nstate[3].on();
           current = 1;
           break;
         }
         case 1: {
-          this.state[0].off();
-          this.state[1].off();
-          this.state[2].on();
-          this.state[3].off();
+          nstate[0].off();
+          nstate[1].off();
+          nstate[2].on();
+          nstate[3].off();
           current = 2;
           break;
         }
         case 2: {
-          this.state[0].off();
-          this.state[1].on();
-          this.state[2].off();
-          this.state[3].off();
+          nstate[0].off();
+          nstate[1].on();
+          nstate[2].off();
+          nstate[3].off();
           current = 3;
           break;
         }
         case 3: {
-          this.state[0].on();
-          this.state[1].off();
-          this.state[2].off();
-          this.state[3].off();
+          nstate[0].on();
+          nstate[1].off();
+          nstate[2].off();
+          nstate[3].off();
           current = 0;
           break;
         }
         default: {
-          this.state[0].off();
-          this.state[1].off();
-          this.state[2].off();
-          this.state[3].on();
+          nstate[0].off();
+          nstate[1].off();
+          nstate[2].off();
+          nstate[3].on();
           current = 0;
           break;
         }
       }
       console.log("Step: ", i, " State: ", current);
       self(i, n);
-    }, 500);
+    }, 300);
   }
 }
 
