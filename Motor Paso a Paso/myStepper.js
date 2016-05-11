f2934f31a204544febd15aaabbcb9923e599d4f3
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-05-11 11:04:13 -0500

    Modify myStepper

--- Motor Paso a Paso/myStepper.js
+++ Motor Paso a Paso/myStepper.js
@@ -1,8 +1,7 @@
 function Stepper(state) {
   var nstate = state;
-  var current = 0;
 
-  this.step = function self(i, n) {
+  this.step = function self(i, n, current) {
     setTimeout(function() {
       if (i == n) return;
       i++;
@@ -39,18 +38,10 @@ function Stepper(state) {
           current = 0;
           break;
         }
-        default: {
-          nstate[0].off();
-          nstate[1].off();
-          nstate[2].off();
-          nstate[3].on();
-          current = 0;
-          break;
-        }
       }
       console.log("Step: ", i, " State: ", current);
-      self(i, n);
-    }, 300);
+      self(i, n, current);
+    }, 0.5);
   }
 }
 
--- Motor Paso a Paso/stepper.js
+++ Motor Paso a Paso/stepper.js
@@ -3,10 +3,10 @@ const myStepper = require('./myStepper');
 const board = new five.Board();
 
 board.on('ready', function() {
-  var led1 = new five.Led(11);
-  var led2 = new five.Led(10);
-  var led3 = new five.Led(9);
-  var led4 = new five.Led(8);
+  var led1 = new five.Led(8);
+  var led2 = new five.Led(9);
+  var led3 = new five.Led(10);
+  var led4 = new five.Led(11);
   var state = [led1, led2, led3, led4];
   var stepper = new myStepper.Stepper(state);
   var button1 = new five.Button(2);
@@ -14,6 +14,6 @@ board.on('ready', function() {
 
   button1.on('press', function() {
     console.log("Start");
-    stepper.step(0, 100);
+    stepper.step(0, 200, 0);
   });
 });
